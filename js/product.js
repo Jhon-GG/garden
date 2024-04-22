4dbfd4dea021eeacb644a3c2891ea8401cd91a46
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-04-22 12:27:48 -0500

    feat: :construction: Actualizacion de trabajo de desconstruccion
    Se realizaron avances en el tema de deconstruccion del 22 de abril

---
+++ js/product.js
@@ -0,0 +1,36 @@
+// const getAllProducts = async() =>{
+//     let peticion = await fetch(import.meta.dirname + "db/product.json");
+//     let data = await peticion.json();
+//     return data;
+// }
+
+// console.log(await getAllProducts());
+
+import fs from 'node:fs';
+
+let contar = (opc = 0, condicion = 3) =>{
+    console.log(opc);
+    let res = (condicion > opc) ? contar(opc + 1) : opx;
+    return (condicion <= 1) ? contar(opc + 1) : opc;
+}
+
+console.log(contar);
+
+
+// fs.readFile(import.meta.dirname + "/db/product.json", 'utf8', (err, data) => {
+//     let json = JSON.parse(data);
+//     let [producto1] = json.products
+//     let gamaNuevo = `${(producto1.gama != "Herramientas") ? producto1.gama : undefined}`
+//     let {description:des} = producto1
+
+//     console.log(gamaNuevo);
+// });
+
+
+
+// Ejemplo de desestructuración
+
+// let saludar = ([nombre, apellido] = info) =>{
+//     return `${nombre} ${apellido}`
+// }
+// console.log(saludar(...["Miguel", "Castro"]));
