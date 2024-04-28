7bd59d3bd71ab0a57b1d91733abdc0a5ccb5f7dc
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-04-28 06:52:38 -0500

    feat: :construction: Ensamblaje de ejercicio 1 de la segunda parte de las consultas en bd
    Se realizó el Ensamblaje de ejercicio 1 de la segunda parte de las consultas en bd

--- js/report.js
+++ js/report.js
@@ -493,6 +493,39 @@ queryAboutTable16.addEventListener("click", async (e) => {
 
 //------------------------- SEGUNDA PARTE ------------------------------------------------------------------------------------------------------------
 
+
+// -----------------------------Ejercicio 1----------------------------------------------------------------
+
+import { getClientsWithNameAndLastNameOfTheSalesManager  } from "./module/clients.js";
+const queryAboutTable01 = document.querySelector("#queryAboutTable01");
+queryAboutTable01.addEventListener("click", async (e) => {
+    let [, report__container] = queryAboutTable01.children;
+    if (!report__container.innerHTML) {
+        let data = await getClientsWithNameAndLastNameOfTheSalesManager ();
+        let plantilla = "";
+        console.log(data);
+        data.forEach(val => {
+            plantilla += `
+                <div class="report__card">
+                    <div class="card__title">
+                        <div>Clientes con representante de ventas</div>
+                    </div>
+                    <div class="card__body">
+                        <div class="body__marck">
+                            <p><b>Cliente Nombre: </b>${val.client_name}</p>
+                            <p><b>Nombre empleado: </b>${val.name_employee }</p>
+
+
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        report__container.innerHTML = plantilla;
+    }
+});
+
+
 //------------------------- Ejercicio 7 ------------------------------------------------------------------------------------------------------------
 
 import { getClientsEmploy } from "./module/clients.js";
